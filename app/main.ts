ef648b7ebb150e9c79abd0b5ab3742f543f3a806
Samrood Ali <devsamrood@gmail.com>
2025-05-02 19:39:31 +0530

    fix: write to socket directly

--- app/main.ts
+++ app/main.ts
@@ -2,9 +2,7 @@ import * as net from "net";
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("ready", () => {
-    socket.write("HTTP 1.1 OK\r\n\r\n");
-  });
+  socket.write("HTTP/1.1 200 OK\r\n\r\n");
 
   socket.on("close", () => {
     socket.end();
