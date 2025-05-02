bca1aed2f8307bebb94bb9fe6bb4eb82d65ff4e1
Samrood Ali <devsamrood@gmail.com>
2025-05-02 19:38:28 +0530

    feat: return 200 response when socket is ready

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,10 @@ import * as net from "net";
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on("ready", () => {
+    socket.write("HTTP 1.1 OK\r\n\r\n");
+  });
+
   socket.on("close", () => {
     socket.end();
   });
