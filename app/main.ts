8b11e5cd9547ab828eb8e344f42aff292dd76d80
Samrood Ali <devsamrood@gmail.com>
2025-05-02 20:09:10 +0530

    fix: parsing path and responding with 200/404

--- app/main.ts
+++ app/main.ts
@@ -2,10 +2,24 @@ import * as net from "net";
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+  socket.on("data", (data) => {
+    const request = data.toString();
 
-  socket.on("close", () => {
-    socket.end();
+    const requestLines = request.split("\r\n");
+    const [_, path, __] = requestLines[0].split(" ");
+
+    console.log(path);
+    switch (path) {
+      case "/": {
+        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        break;
+      }
+      default:
+        {
+          socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+        socket.end();
+    }
   });
 });
 
