7565f7ec2d982db06d49ad76df692a78eb223183
Samrood Ali <devsamrood@gmail.com>
2025-05-04 20:31:55 +0530

    feat: wip recursive route splitter

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,30 @@ import * as net from "net";
 import { requestParser } from "./http/parser";
 import { routes } from "./routes";
 
+function recurser(path: string, acc = {}) {
+  console.log(path, acc);
+  if (path === "") return;
+
+  console.log(path);
+  const slashSplit = path.split("/");
+  console.log(slashSplit);
+  const parent = slashSplit[0];
+  const rest = slashSplit.slice(1).join("/");
+  console.log(parent);
+  console.log(rest);
+  return recurser(rest);
+}
+
+const routeMatcher = (completePath: string) => {
+  // const [reqMethod, reqPath] = completePath.split(" ");
+
+  const allRoutes = Object.keys(routes);
+  allRoutes.forEach((route) => {
+    const [method, path] = route.split(" ");
+    recurser(path);
+  });
+};
+
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
@@ -9,9 +33,9 @@ const server = net.createServer((socket) => {
 
     const requestObject = requestParser(request);
 
-    console.log(requestObject);
     const controller = routes[requestObject.method + " " + requestObject.path];
 
+    routeMatcher(requestObject.method + " " + requestObject.path);
     let response = "";
     if (typeof controller === "function") {
       response = controller(requestObject);
@@ -29,3 +53,5 @@ const server = net.createServer((socket) => {
 
 server.listen(4221, "localhost");
 console.log("Server started listening on port http://localhost:4221");
+
+routeMatcher("GET /echo/abc");
